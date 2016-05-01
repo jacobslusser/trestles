665ab0730ebe0b58c88f260d0f28133e8e77f9e3
Jacob Slusser <jslusser.mobile@gmail.com>
2016-04-30 21:13:33 -0700

    Basic router.

--- src/js/spa.js
+++ src/js/spa.js
@@ -3,4 +3,33 @@
 
     var spa = window.spa = {};
 
+    var router = spa.router = {};
+    (function () {
+        var routes = [];
+
+        function hashChange() {
+            var hash = window.location.hash;
+            for (var i = 0; i < routes.length; i++) {
+                var route = routes[i];
+                var result = route.pattern.exec(hash);
+                if (result) {
+                    route.callback.apply(null, result);
+                    break;
+                }
+            }
+        }
+
+        router.on = function (pattern, callback) {
+            routes.push({
+                pattern: pattern,
+                callback: callback
+            });
+        }
+
+        router.start = function () {
+            window.addEventListener('hashchange', hashChange, false);
+            hashChange();
+        }
+    } ());
+
 } ());
--- src/js/trestles.js
+++ src/js/trestles.js
@@ -3,6 +3,15 @@
 
     var trestles = window.trestles = {};
 
+    // Register routes
+    spa.router.on(/^(#\/)?$/i, function () {
+        console.log('root');
+    });
     
+    spa.router.on(/^#\/signin\/?$/i, function() {
+        console.log('sign-in');
+    });
+
+    spa.router.start();
 
 } ());
