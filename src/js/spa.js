e2463f759c9bdb105b76ae2074c070355f5775f4
Jacob Slusser <jslusser.mobile@gmail.com>
2016-05-01 07:46:45 -0700

    Basic XHR/AJAX support.

--- src/js/spa.js
+++ src/js/spa.js
@@ -3,6 +3,70 @@
 
     var spa = window.spa = {};
 
+    var utils = spa.utils = {};
+    (function () {
+        utils.extend = function (out) {
+            out = out || {};
+
+            for (var i = 1; i < arguments.length; i++) {
+                if (!arguments[i]) {
+                    continue;
+                }
+
+                for (var key in arguments[i]) {
+                    if (arguments[i].hasOwnProperty(key)) {
+                        out[key] = arguments[i][key];
+                    }
+                }
+            }
+
+            return out;
+        };
+    } ());
+
+    var xhr = spa.xhr = {};
+    (function () {
+
+        // Default options
+        var settings = {
+            async: true,
+            dataType: 'text',
+            method: 'GET'
+        }
+
+        xhr.send = function (options, callback) {
+            options = utils.extend(settings, options);
+
+            var request = new XMLHttpRequest();
+            request.open(options.method, options.url, options.async);
+
+            request.onload = function () {
+                if (request.status >= 200 && request.status < 400) {
+                    var data;
+                    switch (options.dataType) {
+                        case 'text':
+                        case 'html':
+                            data = request.responseText;
+                            break;
+                        case 'json':
+                            data = JSON.parse(request.responseText);
+                            break;
+                    }
+                    callback(null, data);
+                }
+                else {
+                    callback('TODO ERROR');
+                }
+            };
+
+            request.onerror = function () {
+                callback('TODO ERROR');
+            };
+
+            request.send();
+        };
+    } ());
+
     var router = spa.router = {};
     (function () {
         var routes = [];
@@ -24,12 +88,33 @@
                 pattern: pattern,
                 callback: callback
             });
-        }
+        };
 
         router.start = function () {
             window.addEventListener('hashchange', hashChange, false);
             hashChange();
-        }
+        };
+    } ());
+
+    var components = spa.components = {};
+    (function () {
+        var components = {};
+        var LOAD_STATE_NONE = 0;
+        var LOAD_STATE_LOADING = 1;
+        var LOAD_STATE_LOADED = 2;
+
+        components.register = function (name, config) {
+            components[name] = utils.extend(config, {
+                loadListeners: [],
+                loadState: LOAD_STATE_NONE
+            });
+        };
+
+        components.load = function (name, callback) {
+            // TODO Look for the constructor function and if not found, load the script
+            // TODO Get the template by ID
+
+        };
     } ());
 
 } ());
--- src/js/trestles.js
+++ src/js/trestles.js
@@ -7,11 +7,19 @@
     spa.router.on(/^(#\/)?$/i, function () {
         console.log('root');
     });
-    
-    spa.router.on(/^#\/signin\/?$/i, function() {
+
+    spa.router.on(/^#\/signin\/?$/i, function () {
         console.log('sign-in');
     });
 
     spa.router.start();
+
+    // HACK for testing XHR
+    /*spa.xhr.send({
+        url: 'components/signin/signin.html',
+        dataType: 'html'
+    }, function(err, data) {
+        console.log(err, data);
+    });*/
 
 } ());
